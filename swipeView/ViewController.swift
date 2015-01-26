c097a4f59b8f758abf71139cbf8b9822e50bbd07
kai <hotsplusph@gmail.com>
2015-01-26 22:22:25 +0900

    Initial Commit

---
+++ swipeView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swipeView
+//
+//  Created by kai ogita on 2015/01/26.
+//  Copyright (c) 2015年 kai ogita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
